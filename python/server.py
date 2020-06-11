69941bca54aeea2502cefcc0b07f879a49c0e984
gcwhitfield <gwhitfieldjr@gmail.com>
2020-06-11 14:03:38 -0400

    add starter code for websocket server

--- python/server.py
+++ python/server.py
@@ -15,10 +15,15 @@ How to use this file:
 
 import socketserver
 import string
+import asyncio
+import websockets
+import string
 
 # starter code is copied from the python documentation about socketserver
 # https://docs.python.org/3/library/socketserver.html
 
+
+# ------------ old server code (regular socket connection) ------------------- #
 class AttentionTaskState:
     def __init__(self, researcher, child):
         self.researcher = researcher # socket connection
@@ -73,7 +78,7 @@ class AttentionTaskServer(socketserver.BaseRequestHandler):
 2) reads the data that is sent to the sevrer
 3) sends the same data back to the client, but uppercased
 '''
-def main():
+def main_old():
     if __name__ == "__main__":
         HOST, PORT = "localhost", 9999
         # connect the server to localhost on port 9999
@@ -92,4 +97,19 @@ Sends a message to the researcher and the child once they both have confirmed.
 def wait_confirm(server):
     pass
 
-main()
+
+# ------------------- new server code (websockets) --------------------------- #
+
+# websockets code copied from websockets documentation
+# https://websockets.readthedocs.io/en/stable/intro.html
+
+async def server(websocket, path):
+    msg = await websocket.recv()
+    print(f"Here is the msg: {msg}")
+    response = msg.upper()
+    await websocket.send(response) 
+
+def main():
+    start_server = websockets.serve(server, "localhost", 1234)
+    asyncio.get_event_loop().run_until_complete(start_server)
+    asyncio.get_event_loop().run_forever()
